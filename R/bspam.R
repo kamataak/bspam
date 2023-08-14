dbea993f4fef1e4079326244a443c600754188cd
Kuo Wang <wangkuo@nifty.com>
2023-08-14 16:31:16 -0500

    Add bspam-package file for PDF manual

---
+++ R/bspam.R
@@ -0,0 +1,21 @@
+#' bspam : A package for fitting the speed-accuracy psychometric model for count outcome data.
+#'
+#' @description 
+#' \strong{Purpose of this package} 
+#' 
+#' Write something here 
+#' 
+#' \strong{Design philosophy} 
+#' 
+#' Write Design philosophy
+#' 
+#' @author Akihito Kamata <akamata@smu.edu>
+#' @author Cornelis J. Potgieter <c.potgieter@tcu.edu>
+#' @author Joseph F. T. Nese <jnese@uoregon.edu>
+#' @author Yusuf Kara <ykara@mail.smu.edu>
+#' @author Sarunya Somsong <sarunya.ss@hotmail.com>
+#' @author Kuo Wang <wangkuo@nifty.com>
+#'  
+#' @docType package
+#' @name bspam-package
+NULL
