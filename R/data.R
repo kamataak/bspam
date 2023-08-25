5015245967dbd6681a37bcefd863d9a6b727048e
Kuo Wang <wangkuo@nifty.com>
2023-08-24 22:18:00 -0500

    Add two calibration data sets.

--- R/data.R
+++ R/data.R
@@ -18,3 +18,61 @@
 #' }
 #' @source \url{https://jnese.github.io/core-blog/}
 "passage2"
+
+#' @title Passage calibration data with MCEM
+#'
+#' @description A data set calibrated with MCEM.
+#'
+#' @format two lists: $pass.param with 150 rows and $hyper.param with 4 variables
+#' @format $pass.param
+#' \describe{
+#'   \item{a}{parameter controlling binomial success probabilities}
+#'   \item{b}{parameter controlling binomial success probabilities}
+#'   \item{alpha}{parameter controlling reading times}
+#'   \item{beta}{parameter controlling reading times}
+#'   \item{se_a}{standard error of a}
+#'   \item{se_b}{standard error of b}
+#'   \item{se_alpha}{standard error of alpha}
+#'   \item{se_beta}{standard error of beta}
+#'   \item{passage.id}{passage ID}
+#'   \item{nwords.p}{the total }
+#' }
+#' @format $hyper.param
+#' \describe{
+#'   \item{vartau}{variance of latent reading ability tau}
+#'   \item{rho}{correlation between two latent variables}
+#'   \item{se_vartau}{standard error of vartar}
+#'   \item{se_rho}{standard error of rho}
+#' }
+#' @source \url{https://jnese.github.io/core-blog/}
+"passage.calib.mcem"
+
+#' @title Passage calibration data with bayes
+#'
+#' @description A data set calibrated with bayes.
+#'
+#' @format two lists: $pass.param with 150 rows and $hyper.param with 4 variables
+#' @format $pass.param
+#' \describe{
+#'   \item{a}{parameter controlling binomial success probabilities}
+#'   \item{b}{parameter controlling binomial success probabilities}
+#'   \item{alpha}{parameter controlling reading times}
+#'   \item{beta}{parameter controlling reading times}
+#'   \item{se_a}{standard error of a}
+#'   \item{se_b}{standard error of b}
+#'   \item{se_alpha}{standard error of alpha}
+#'   \item{se_beta}{standard error of beta}
+#'   \item{passage.id}{passage ID}
+#'   \item{nwords.p}{the total }
+#' }
+#' @format $hyper.param
+#' \describe{
+#'   \item{vartau}{variance of latent reading ability tau}
+#'   \item{rho}{correlation between two latent variables}
+#'   \item{se_vartau}{standard error of vartar}
+#'   \item{se_rho}{standard error of rho}
+#' }
+#' @source \url{https://jnese.github.io/core-blog/}
+"passage.calib.bayes"
+
+
