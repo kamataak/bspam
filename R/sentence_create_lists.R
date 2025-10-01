63e267ce10c6b8c14748bbf1de1a2a171fdacf54
Yusuf Kara <ykara@miami.edu>
2025-09-30 23:44:53 -0400

    Extended the  create data list function to allow the external scoring.

--- R/sentence_create_lists.R
+++ R/sentence_create_lists.R
@@ -39,9 +39,10 @@
 #' censored (1) or fully observed (0) -- K-dim
 #' 
 #' @return  list
-create_data_list_sentence <- function(Count, logT10, MaxN, 
-                             Passage, a, b, 
-                             alpha, beta, 
+create_data_list_sentence <- function(Count, logT10, MaxN, MaxN_score,
+                             Passage, Passage_score, a, b, 
+                             alpha, beta,
+                             a_score, b_score, alpha_score, beta_score,
                              gamma1, gamma2, 
                              sigma, rho, rhoTestlet, 
                              C) {
@@ -78,23 +79,33 @@ create_data_list_sentence <- function(Count, logT10, MaxN,
   data_list <- list(
     N_obs = N_obs,
     N_cens = N_cens,
+    N_score = length(MaxN_score),
     K = max(Passage),
+    K_score = max(Passage_score),
     Passage_obs = safe_na_omit(Passage[C == 0]),
     Passage_cens = safe_na_omit(Passage[C == 1]),
+    Passage_score = Passage_score, 
     Count_obs = na.omit(Count[C == 0]),
     Count_cens = na.omit(Count[C == 1]),
     logT10_obs = na.omit(logT10[C == 0]),
     logT10_cens = na.omit(logT10[C == 1]),
     MaxN_obs = safe_na_omit(MaxN[C == 0]),
     MaxN_cens = safe_na_omit(MaxN[C == 1]),
+    MaxN_score = MaxN_score,
+    #Note: for the parameters of the passages to be scored, does it matter to account for censoring? I don't think soo. but just wanted to ask.
+    #Note: the stan code for the testlet model does not use the _cens params! No need to worry about those. 
     a_obs = na.omit(a[C == 0]),
     a_cens = na.omit(a[C == 1]),
+    a_score=a_score,
     b_obs = na.omit(b[C == 0]),
     b_cens = na.omit(b[C == 1]),
+    b_score=b_score,
     alpha_obs = na.omit(alpha[C == 0]),
     alpha_cens = na.omit(alpha[C == 1]),
+    alpha_score=alpha_score,
     beta_obs = na.omit(beta[C == 0]),
     beta_cens = na.omit(beta[C == 1]),
+    beta_score=beta_score,
     gamma1 = gamma1,
     gamma2 = gamma2,
     sigma = sigma,
@@ -140,20 +151,28 @@ score_testlet_sentence <- function(data_list) {
     posterior_sd <- summary_info$summary[, "sd"]
     theta1_sd <- posterior_sd["theta1"]
     theta2_sd <- posterior_sd["theta2"]
-  } else if (C0 >= 2 && C1 == 0) {
+  } else if (C0 >= 2 && C1 == 0) { ## -----> This is the testlet model!!!
     fit <- rstan::sampling(model_multi_obs_no_cens_sentence, #model_no_obs_multi_cens_sentence, 
                     data = data_list, chains = 4, 
                     iter = 2000, warmup = 1000)
     summary_info <- rstan::summary(fit)
     # Extract posterior means
     posterior_means <- summary_info$summary[, "mean"]
-    # Extract the means for theta1 and theta2
+    # Extract the means for theta1 and theta2 + scoring params
     theta1_mean <- posterior_means["theta1"]
     theta2_mean <- posterior_means["theta2"]
-    # Extract the sd's for theta1 and theta2
+    count_mean <- posterior_means["exp_cnt"]
+    time_mean <- posterior_means["exp_tim"]
+    wcpm_mean <- posterior_means["wcpm"]
+    
+    # Extract the sd's for theta1 and theta2 + scoring params
     posterior_sd <- summary_info$summary[, "sd"]
     theta1_sd <- posterior_sd["theta1"]
     theta2_sd <- posterior_sd["theta2"]
+    count_sd <- posterior_sd["exp_cnt"]
+    time_sd <- posterior_sd["exp_tim"]
+    wcpm_sd <- posterior_sd["wcpm"]
+    
   } else if (C0 == 1 && C1 >= 2) { # C1 is cens, C0 is obs
     fit <- rstan::sampling(model_one_obs_multi_cens_sentence, #model_multi_obs_one_cens_sentence, 
                     data = data_list, chains = 4, 
@@ -201,6 +220,13 @@ score_testlet_sentence <- function(data_list) {
   return(list(theta_acc = as.numeric(theta1_mean),
               theta_spd = as.numeric(theta2_mean),
               theta_acc_sd = as.numeric(theta1_sd),
-              theta_spd_sd = as.numeric(theta2_sd)))
+              theta_spd_sd = as.numeric(theta2_sd), 
+              count=as.numeric(count_mean), 
+              time=as.numeric(time_mean), 
+              wcpm=as.numeric(wcpm_mean), 
+              count_sd=as.numeric(count_sd),
+              time_sd=as.numeric(time_sd),
+              wcpm_sd=as.numeric(wcpm_sd))
+         )
   
 }
