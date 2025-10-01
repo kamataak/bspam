a4b7516e74f7a3b87f052ed93253c248d0b0b83d
Yusuf Kara <ykara@miami.edu>
2025-09-30 23:45:53 -0400

    Revised the function to add the external scoring and mdoel-based estimation options.

--- R/scoring_sentence_censoring.R
+++ R/scoring_sentence_censoring.R
@@ -39,10 +39,27 @@
 #' censored (1) or fully observed (0) -- K-dim
 #' 
 #' @return  list
-scoring.sentence.censoring <- function(Count=NULL, logT10=NULL, N=NULL, 
-                                        Passage=NULL, a=NULL, b=NULL, alpha=NULL,beta=NULL, 
-                                        gamma1=NULL, gamma2=NULL,
-                                        sigma=NULL, rho=NULL, rhoTestlet=NULL, C=NULL) {
+scoring.sentence.censoring <- function(Count=NULL, 
+                                       logT10=NULL, 
+                                       N=NULL, 
+                                       N_score=NULL,
+                                       Passage=NULL, 
+                                       Passage_score=NULL, 
+                                       a=NULL,
+                                       a_score=NULL,
+                                       b=NULL,
+                                       b_score=NULL,
+                                       alpha=NULL,
+                                       alpha_score=NULL,
+                                       beta=NULL,
+                                       beta_score=NULL,
+                                       gamma1=NULL, 
+                                       gamma2=NULL,
+                                       sigma=NULL, 
+                                       rho=NULL, 
+                                       rhoTestlet=NULL, 
+                                       C=NULL, 
+                                       type=NULL) {
   
   # loading logger
   log.initiating()
@@ -59,15 +76,22 @@ scoring.sentence.censoring <- function(Count=NULL, logT10=NULL, N=NULL,
   logT10 <- data.matrix(logT10) # [r,c]
   Cens <- data.matrix(C)
   
-  # get Passage orders
+  # Get orders & sequence for passages READ
   unique_id <- Passage %>% unique()
-  
-  # Get passage sequence
   passage_seq <- Passage
   
   for (i in 1:length(unique_id)) {
     passage_seq[passage_seq == unique_id[i]] <- i
   }
+  
+  # Get orders & sequence for passages to be SCORED
+  unique_id_score <- Passage_score %>% unique()
+  passage_seq_score <- Passage_score
+  
+  for (i in 1:length(unique_id_score)) {
+    passage_seq_score[passage_seq_score == unique_id_score[i]] <- i
+  }
+  
   
   # person number
   n = dim(Cens)[[1]]
@@ -76,18 +100,39 @@ scoring.sentence.censoring <- function(Count=NULL, logT10=NULL, N=NULL,
   theta_spd_est <-rep(0,n)
   theta_acc_sd <-rep(0,n)
   theta_spd_sd <-rep(0,n)
+  count_est <-rep(0,n)
+  time_est <-rep(0,n)
+  wcpm_est <-rep(0,n)
+  count_sd <-rep(0,n)
+  time_sd <-rep(0,n)
+  wcpm_sd <-rep(0,n)
   
   for (k in 1:n) {
     
     # for debug
     # print(paste(" k= ", k))
     
-    data_list <- create_data_list_sentence(Y[k,], logT10[k,], N, 
-                                            passage_seq, a, b, 
-                                            alpha, beta, 
-                                            gamma1, gamma2,
-                                            sigma, rho, rhoTestlet,
-                                            Cens[k,])
+    #each k slices the kth row of the examinee x sentence data!
+    data_list <- create_data_list_sentence(Count=Y[k,], 
+                                           logT10=logT10[k,], 
+                                           MaxN=N,
+                                           MaxN_score=N_score,
+                                           Passage=passage_seq,
+                                           Passage_score=passage_seq_score,
+                                           a=a, 
+                                           b=b, 
+                                           alpha=alpha, 
+                                           beta=beta,
+                                           a_score=a_score,
+                                           b_score=b_score, 
+                                           alpha_score=alpha_score,
+                                           beta_score=beta_score,
+                                           gamma1=gamma1, 
+                                           gamma2=gamma2,
+                                           sigma=sigma, 
+                                           rho=rho, 
+                                           rhoTestlet=rhoTestlet,
+                                           C=Cens[k,])
   
     suppress_output({
       score_object <- score_testlet_sentence(data_list)
@@ -97,13 +142,36 @@ scoring.sentence.censoring <- function(Count=NULL, logT10=NULL, N=NULL,
     theta_spd_est[k] <- score_object$theta_spd
     theta_acc_sd[k] <- score_object$theta_acc_sd
     theta_spd_sd[k] <- score_object$theta_spd_sd
+    time_est[k] <- score_object$time
+    count_est[k] <- score_object$count
+    wcpm_est[k] <- score_object$wcpm
+    time_sd[k] <- score_object$time_sd
+    count_sd[k] <- score_object$count_sd
+    wcpm_sd[k] <- score_object$wcpm_sd
       
   }
   
   flog.info("End scoring sentence censoring process", name = "orfrlog")
   
+  if(type=="general"){
   return(list(theta_acc_est=theta_acc_est,
               theta_spd_est=theta_spd_est,
               theta_acc_sd=theta_acc_sd,
-              theta_spd_sd=theta_spd_sd))
+              theta_spd_sd=theta_spd_sd, 
+              count_est=count_est, 
+              time_est=time_est,
+              count_sd=count_sd, 
+              time_sd=time_sd))
+  }else if(type=="orf"){
+    return(list(theta_acc_est=theta_acc_est,
+                theta_spd_est=theta_spd_est,
+                theta_acc_sd=theta_acc_sd,
+                theta_spd_sd=theta_spd_sd, 
+                count_est=count_est, 
+                time_est=time_est,
+                count_sd=count_sd, 
+                time_sd=time_sd, 
+                wcpm_est=wcpm_est, 
+                wcpm_sd=wcpm_sd))
+  }
 }
