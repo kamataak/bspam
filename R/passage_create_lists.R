c68a7d5669f46ef539b6197f950abb7bf3fc8765
Yusuf Kara <ykara@miami.edu>
2026-01-17 09:52:34 -0500

    Added code for passage-level censored data scoring capability.

--- R/passage_create_lists.R
+++ R/passage_create_lists.R
@@ -35,31 +35,47 @@
 #' @param C: A vector of indicators whether a specific passage was 
 #' censored (1) or fully observed (0) -- K-dim
 #'  
-create_data_list <- function(Count, logT10, MaxN, 
-                             a, b, alpha,beta, 
+create_data_list <- function(Count, logT10, MaxN, MaxN_score,
+                             a, b, alpha,beta,
+                             a_score, b_score, alpha_score, beta_score,
                              sigma, rho, C) {
   # Calculate the number of observed and censored passages
   N_obs <- sum(C == 0, na.rm = TRUE) # try na.rm = T, 04/07/24 by Kuo
   N_cens <- sum(C == 1, na.rm = TRUE) # try na.rm = T, 04/07/24 by Kuo
   
+  #Attach dim spec to prevent stan issues when only 1 task is used for scoring.
+  N_score = length(MaxN_score)
+  dim(MaxN_score) <- N_score
+  
+  dim(a_score) <- N_score
+  dim(b_score) <- N_score
+  dim(alpha_score) <- N_score
+  dim(beta_score) <- N_score
+  
   # Create a list containing subsets of input data
   data_list <- list(
     N_obs = N_obs,
     N_cens = N_cens,
+    N_score = N_score,
     Count_obs = na.omit(Count[C == 0]),
     Count_cens = na.omit(Count[C == 1]),
     logT10_obs = na.omit(logT10[C == 0]),
     logT10_cens = na.omit(logT10[C == 1]),
     MaxN_obs = na.omit(MaxN[C == 0]),
     MaxN_cens = na.omit(MaxN[C == 1]),
+    MaxN_score = MaxN_score,
     a_obs = na.omit(a[C == 0]),
     a_cens = na.omit(a[C == 1]),
+    a_score=a_score,
     b_obs = na.omit(b[C == 0]),
     b_cens = na.omit(b[C == 1]),
+    b_score=b_score,
     alpha_obs = na.omit(alpha[C == 0]),
     alpha_cens = na.omit(alpha[C == 1]),
+    alpha_score=alpha_score,
     beta_obs = na.omit(beta[C == 0]),
     beta_cens = na.omit(beta[C == 1]),
+    beta_score=beta_score,
     sigma = sigma,
     rho = rho
   )
@@ -89,10 +105,19 @@ score_testlet <- function(data_list) {
     # Extract the means for theta1 and theta2
     theta1_mean <- posterior_means["theta1"]
     theta2_mean <- posterior_means["theta2"]
+    count_mean <- posterior_means["exp_cnt"]
+    time_mean <- posterior_means["exp_tim"]
+    wcpm_mean <- posterior_means["wcpm"]
+    
     # Extract the sd's for theta1 and theta2
     posterior_sd <- summary_info$summary[, "sd"]
     theta1_sd <- posterior_sd["theta1"]
     theta2_sd <- posterior_sd["theta2"]
+    count_sd <- posterior_sd["exp_cnt"]
+    time_sd <- posterior_sd["exp_tim"]
+    wcpm_sd <- posterior_sd["wcpm"]
+    
+    
   } else if (C0 >= 2 && C1 == 1) { # C0 is obs, C1 is cens
     fit <- rstan::sampling(model_multi_obs_one_cens, # model_one_obs_multi_cens, 
                     data = data_list, chains = 4, 
@@ -103,11 +128,19 @@ score_testlet <- function(data_list) {
     # Extract the means for theta1 and theta2
     theta1_mean <- posterior_means["theta1"]
     theta2_mean <- posterior_means["theta2"]
+    count_mean <- posterior_means["exp_cnt"]
+    time_mean <- posterior_means["exp_tim"]
+    wcpm_mean <- posterior_means["wcpm"]
+    
     # Extract the sd's for theta1 and theta2
     posterior_sd <- summary_info$summary[, "sd"]
     theta1_sd <- posterior_sd["theta1"]
     theta2_sd <- posterior_sd["theta2"]
-  } else if (C0 >= 2 && C1 == 0) { # C0 is obs, C1 is cens
+    count_sd <- posterior_sd["exp_cnt"]
+    time_sd <- posterior_sd["exp_tim"]
+    wcpm_sd <- posterior_sd["wcpm"]
+    
+  } else if (C0 >= 2 && C1 == 0) { # C0 is obs, C1 is cens 
     fit <- rstan::sampling(model_multi_obs_no_cens, #model_no_obs_multi_cens, 
                     data = data_list, chains = 4, 
                     iter = 2000, warmup = 1000)
@@ -117,11 +150,19 @@ score_testlet <- function(data_list) {
     # Extract the means for theta1 and theta2
     theta1_mean <- posterior_means["theta1"]
     theta2_mean <- posterior_means["theta2"]
+    count_mean <- posterior_means["exp_cnt"]
+    time_mean <- posterior_means["exp_tim"]
+    wcpm_mean <- posterior_means["wcpm"]
+    
     # Extract the sd's for theta1 and theta2
     posterior_sd <- summary_info$summary[, "sd"]
     theta1_sd <- posterior_sd["theta1"]
     theta2_sd <- posterior_sd["theta2"]
-  } else if (C0 == 1 && C1 >= 2) { # C0 is obs, C1 is cens
+    count_sd <- posterior_sd["exp_cnt"]
+    time_sd <- posterior_sd["exp_tim"]
+    wcpm_sd <- posterior_sd["wcpm"]
+    
+    } else if (C0 == 1 && C1 >= 2) { # C0 is obs, C1 is cens
     fit <- rstan::sampling(model_one_obs_multi_cens, #model_multi_obs_one_cens, 
                     data = data_list, chains = 4, 
                     iter = 2000, warmup = 1000)
@@ -131,10 +172,19 @@ score_testlet <- function(data_list) {
     # Extract the means for theta1 and theta2
     theta1_mean <- posterior_means["theta1"]
     theta2_mean <- posterior_means["theta2"]
+    count_mean <- posterior_means["exp_cnt"]
+    time_mean <- posterior_means["exp_tim"]
+    wcpm_mean <- posterior_means["wcpm"]
+    
+    
     # Extract the sd's for theta1 and theta2
     posterior_sd <- summary_info$summary[, "sd"]
     theta1_sd <- posterior_sd["theta1"]
     theta2_sd <- posterior_sd["theta2"]
+    count_sd <- posterior_sd["exp_cnt"]
+    time_sd <- posterior_sd["exp_tim"]
+    wcpm_sd <- posterior_sd["wcpm"]
+    
   } else if (C0 == 0 && C1 >= 2) { # C0 is obs, C1 is cens
     fit <- rstan::sampling(model_no_obs_multi_cens, #model_multi_obs_no_cens, 
                     data = data_list, chains = 4, 
@@ -145,10 +195,18 @@ score_testlet <- function(data_list) {
     # Extract the means for theta1 and theta2
     theta1_mean <- posterior_means["theta1"]
     theta2_mean <- posterior_means["theta2"]
+    count_mean <- posterior_means["exp_cnt"]
+    time_mean <- posterior_means["exp_tim"]
+    wcpm_mean <- posterior_means["wcpm"]
+    
     # Extract the sd's for theta1 and theta2
     posterior_sd <- summary_info$summary[, "sd"]
     theta1_sd <- posterior_sd["theta1"]
     theta2_sd <- posterior_sd["theta2"]
+    count_sd <- posterior_sd["exp_cnt"]
+    time_sd <- posterior_sd["exp_tim"]
+    wcpm_sd <- posterior_sd["wcpm"]
+    
   } else if (C0 == 1 && C1 == 1) { # C0 is obs, C1 is cens
     fit <- rstan::sampling(model_one_obs_one_cens, 
                     data = data_list, chains = 4, 
@@ -159,10 +217,20 @@ score_testlet <- function(data_list) {
     # Extract the means for theta1 and theta2
     theta1_mean <- posterior_means["theta1"]
     theta2_mean <- posterior_means["theta2"]
+    count_mean <- posterior_means["exp_cnt"]
+    time_mean <- posterior_means["exp_tim"]
+    wcpm_mean <- posterior_means["wcpm"]
+    
+  
     # Extract the sd's for theta1 and theta2
     posterior_sd <- summary_info$summary[, "sd"]
     theta1_sd <- posterior_sd["theta1"]
     theta2_sd <- posterior_sd["theta2"]
+    count_sd <- posterior_sd["exp_cnt"]
+    time_sd <- posterior_sd["exp_tim"]
+    wcpm_sd <- posterior_sd["wcpm"]
+  }else{
+    stop("Error: Your data contain cases with less than two task-level observations, possibly, due to missingness. Consider discarding these cases and rerun the analyses.")
   }
   
   # Returns estimated posterior means and standard deviations
@@ -170,6 +238,12 @@ score_testlet <- function(data_list) {
   return(list(theta_acc = as.numeric(theta1_mean),
               theta_spd = as.numeric(theta2_mean),
               theta_acc_sd = as.numeric(theta1_sd),
-              theta_spd_sd = as.numeric(theta2_sd)))
+              theta_spd_sd = as.numeric(theta2_sd), 
+              count=as.numeric(count_mean), 
+              time=as.numeric(time_mean), 
+              wcpm=as.numeric(wcpm_mean), 
+              count_sd=as.numeric(count_sd),
+              time_sd=as.numeric(time_sd),
+              wcpm_sd=as.numeric(wcpm_sd)))
   
 }
