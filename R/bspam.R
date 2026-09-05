ae8a921b25f6a2e814b5ef705419bb1c5a6001ea
wangkuo08 <wangp@smu.edu>
2026-09-04 23:46:44 -0500

    Update for CRAN check.

--- R/bspam.R
+++ R/bspam.R
@@ -1,17 +1,12 @@
 #' bspam : A package for fitting the speed-accuracy psychometric model for repeatedly measured count outcome data.
 #'
 #' @description 
-#' \strong{Purpose of this package} 
 #' 
 #' `bspam` is an R package that contains functions to fit the speed-accuracy 
 #' psychometric model for repeatedly measured count outcome data (Potgieter, Kamata & Kara, 2017; 
 #' Kara, Kamata, Potgieter & Nese, 2020), where the accuracy is modeled by a binomial 
 #' count latent variable model. For example, the use of this modeling technique 
 #' allows model-based calibration and scoring for oral reading fluency (ORF) assessment data. 
-#' 
-#' \strong{Design philosophy} 
-#' 
-#' Write Design philosophy
 #' 
 #' @importFrom stats cov dbinom dnorm na.omit optim pnorm qnorm rbinom rnorm runif sd time uniroot var 
 #' @importFrom stringr str_split
